10ba95ad7d5d0f9107c953cc8348c2be21f44399
Prateek Kumar <114415426+prateekkumaryadav@users.noreply.github.com>
2022-11-20 00:37:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 var exphbs  = require('express-handlebars');
 const path = require('path')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
@@ -10,6 +10,6 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname,"public")))
 app.use("/",require(path.join(__dirname,"routes/webroute.js")))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port www.localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port www.localhost:${PORT}`)
 })
